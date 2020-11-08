0465698aa30f33254d0ff0b78b5d8c8f430c7452
sunepoulsen <sunepoulsen@gmail.com>
2020-11-09 00:05:58 +0100

    DT-4: Created base project with a small test script.

---
+++ scripts/dt-base64/src/main/groovy/dk/sunepoulsen/devtools/scripts/base64/Base64Application.groovy
@@ -0,0 +1,9 @@
+package dk.sunepoulsen.devtools.scripts.base64
+
+class Base64Application {
+    static void main(String[] args) {
+        println('dt-base64 version 0.1.0-SNAPSHOT')
+        println()
+        println("Arguments: ${args}")
+    }
+}
