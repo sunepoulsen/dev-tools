897615eb31265710e23a0ce5d4476e3ec93a2d58
Sune Thomas Poulsen <Sune.Poulsen@ufst.dk>
2020-11-10 18:31:04 +0100

    Add dt-xml project

---
+++ scripts/dt-xml/src/main/groovy/dk/sunepoulsen/devtools/scripts/xml/ScriptApplication.groovy
@@ -0,0 +1,9 @@
+package dk.sunepoulsen.devtools.scripts.xml
+
+class ScriptApplication {
+    static void main(String[] args) {
+        println('dt-xml version 0.1.0-SNAPSHOT')
+        println()
+        println("Arguments: ${args}")
+    }
+}
